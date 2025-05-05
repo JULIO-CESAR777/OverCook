d77bc7149fc651c04b170e867f5866cde5c27f3d
Julio Cesar- intelamd <125276791+JULIO-CESAR777@users.noreply.github.com>
2025-05-05 08:21:51 -0600

    PushingError
    Borre cositas uwu

--- Assets/MedellinJulio/CocinaGame/TrashBin.cs
+++ Assets/MedellinJulio/CocinaGame/TrashBin.cs
@@ -2,13 +2,10 @@ using UnityEngine;
 
 public class TrashBin : MonoBehaviour
 {
-    public string tag1 = "Ingredient";
-    public string tag2 = "Plate";
-    public string tag3 = "Recipe";
-
+  
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag(tag1) || other.CompareTag(tag2) || other.CompareTag(tag3))
+        if (!other.CompareTag("Player") )
         {
             Destroy(other.gameObject);
             Debug.Log($"{other.name} fue destruido por el bote de basura.");
