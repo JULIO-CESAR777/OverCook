db60d7d86103b4e8c7c7f37da68e9ec3fa21678c
gushabo <113316441+gushabo@users.noreply.github.com>
2025-05-05 18:24:00 -0600

    Changes 4 VR
    Main menu

--- Assets/VrDetector.cs
+++ Assets/VrDetector.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.XR.Management;
 
 public class VrDetector : MonoBehaviour
 {
+
+    [SerializeField] public GameObject Canvas;
+    [SerializeField] public GameObject Camera;
+
+    [SerializeField] public GameObject CanvasVR;
+    [SerializeField] public GameObject Player;
+
+
     void Start()
     {
         var xrManager = XRGeneralSettings.Instance.Manager;
@@ -11,11 +20,31 @@ public class VrDetector : MonoBehaviour
         {
             Debug.Log("XR is initialized and active.");
             // Lógica VR
+            Canvas.SetActive(false);
+            Camera.SetActive(false);
+
+            CanvasVR.SetActive(true);
+            Player.SetActive(true);
         }
         else
         {
             Debug.Log("XR is not active.");
             // Lógica no-VR
+            Canvas.SetActive(true);
+            Camera.SetActive(true);
+
+            CanvasVR.SetActive(false);
+            Player.SetActive(false);
         }
     }
+
+    public void StartGame(){
+        SceneManager.LoadScene("Cooking");
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+
 }
