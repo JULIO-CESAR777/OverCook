2c8c7bb7f14d0ae131d0cb326dff082529fc71a5
Julio Cesar- intelamd <125276791+JULIO-CESAR777@users.noreply.github.com>
2025-05-29 10:54:45 -0600

    Mapa

--- Assets/MedellinJulio/CocinaGame/TrashBin.cs
+++ Assets/MedellinJulio/CocinaGame/TrashBin.cs
@@ -5,10 +5,10 @@ public class TrashBin : MonoBehaviour
     
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Ingredient") || other.CompareTag("Recipe") || other.CompareTag("Plate"))
+        if (other.CompareTag("Ingredient") || other.CompareTag("Recipe") || other.CompareTag("Plate") || other.CompareTag("Pan"))
         {
             Destroy(other.gameObject);
-            Debug.Log($"{other.name} fue destruido por el bote de basura.");
+          
         }
     }
 }
